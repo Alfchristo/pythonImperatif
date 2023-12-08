8c78f41425522af8fb06d452febbbc0fbfc381ca
Alfchristo <juppchristophe@hotmail.com>
2023-12-08 15:43:46 +0100

    - Migration de TP 5 aux Fichier TP 5 Ancienne Version

--- TP 5 Ancienne Version/ex02_InversionContenu.py
+++ TP 5 Ancienne Version/ex02_InversionContenu.py
@@ -7,3 +7,10 @@ for i in reversed(liste):
 
 print("Affichage ordre inverse:")
 print(liste)
+
+
+liste=[1,15,-3,0,8,7,4,-2,28,7,-1,17,2,3,0,14,-4]
+liste.reverse()
+print("l’ensemble des éléments de la liste listeCopie")
+listeCopie = liste.copy()
+print(listeCopie)
--- bases_algorithmie.py
+++ bases_algorithmie.py
@@ -13,10 +13,12 @@ liste = [1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4]
 # Affichage des valeurs dans la liste:
 for i in range(len(liste)):
     print("Affichage de la liste :", liste[i])
+
 # Affichage de valeurs positifs avec une autre méthode:
 for listeValues in liste:
     if listeValues > 0:
         print("Valeurs positifs :", listeValues)
+
 # Calculs du nombre de valeurs positifs dans la liste:
 valeursPositifs = 0
 for listeValues in liste:
