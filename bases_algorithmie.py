593def7b4dabad48b4b8ea51d1f415a7ce8ca2e1
Alfchristo <juppchristophe@hotmail.com>
2023-12-08 11:04:39 +0100

    - TP 4

---
+++ bases_algorithmie.py
@@ -0,0 +1,28 @@
+# Exercice 1
+for i in range(0, 11):
+    if i % 2 ==0:
+        print(i)
+
+for i in range(0, 11):
+    if i % 2 !=0:
+        print(i)
+
+# Exercice 2
+liste = [1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4]
+
+# Affichage des valeurs dans la liste:
+for i in range(len(liste)):
+    print("Affichage de la liste :", liste[i])
+# Affichage de valeurs positifs avec une autre méthode:
+for listeValues in liste:
+    if listeValues > 0:
+        print("Valeurs positifs :", listeValues)
+# Calculs du nombre de valeurs positifs dans la liste:
+valeursPositifs = 0
+for listeValues in liste:
+    if listeValues > 0:
+        valeursPositifs += 1
+print("Compte de valeurs positifs:", valeursPositifs)
+
+
+
