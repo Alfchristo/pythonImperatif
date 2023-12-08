3b9dcff604935bdaa567c6c6cd35f782e5312ec7
Alfchristo <juppchristophe@hotmail.com>
2023-12-08 10:31:37 +0100

    - TP 2

---
+++ declaration_variables.py
@@ -0,0 +1,16 @@
+# Variable int
+variable_int = 15
+print(variable_int, type(variable_int))
+
+# Variable float
+variable_float = 3.5
+print(variable_float, type(variable_float))
+
+# Variable str
+variable_str = "Hello Friend"
+print(variable_str, type(variable_str))
+
+# Variable bool
+variable_bool = True
+print(variable_bool, type(variable_bool))
+
